7fe449bbfd91985c743eb988760d4b22f792e63b
Connor Callahan <connorwcallahan@gmail.com>
2019-04-15 13:28:35 -0400

    render form change

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,12 +2,40 @@ import React, { PureComponent } from 'react';
 
 class Calculator extends PureComponent {
 
+  state = {
+    income: 0,
+    net: null,
+    tax: null,
+    housing: null,
+    transportation: null,
+    utilities: null,
+    insurance: null,
+    savings: null,
+    balacne: 0
+  }
+
+  handleChange = (event) => {
+    if(this.state.income <= 0 && event.target.id != "income") {
+      window.alert('Please enter income first!')
+    } else {
+      this.setState({
+        [event.target.id]:event.target.value
+      })
+      this.renderFormChange()
+    }
+  }
+
+  renderFormChange = () => {
+
+  }
+
   render() {
+    console.log(this.state.income)
     return (
       <div id="calculator">
-        <form>
+        <form onChange={this.handleChange}>
         <label> Net Monthly Income </label>
-        $<input type="number"></input>
+        $<input id="income" type="number"></input>
           <label> Deduct </label>
             %<input type="number"></input>
           <label> For Tax </label>
@@ -30,6 +58,7 @@ class Calculator extends PureComponent {
         <label> Misc. </label>
           <input type="number"></input>
         </form>
+        <h1> Discretionary Income: {this.state.balance}</h1>
       </div>
     );
   }
